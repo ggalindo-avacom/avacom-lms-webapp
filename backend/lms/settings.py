cca37a2a175e79f18758cdac26bd636dbe6a201b
Gabriel Galindo <gabriel.galindo@avacomworld.com>
2026-07-14 17:26:53 -0500

    Configurando Settings: Instalando apps para permitir el acceso desde React en CORS

--- backend/lms/settings.py
+++ backend/lms/settings.py
@@ -27,6 +27,13 @@ DEBUG = True
 
 ALLOWED_HOSTS =  ['localhost', '127.0.0.1', '0.0.0.0']
 
+#CORS ALLOWED HOST
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
+    '*'
+]
+
 
 # Application definition
 
